c39a8f61b79056dacff3ad4b7bf7ef23b443a8d4
constanzamorales <constanzabmorales@gmail.com>
2022-11-09 19:53:24 -0300

    Calling TV Maze API to show the image of the first show the user searches

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const form = document.querySelector("searchForm");
-form.addEventListener("submit", function (e) {
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
 	e.preventDefault();
-	console.dir(form);
+	const searchTerm = form.elements.query.value;
+	const response = await axios.get(
+		`http://api.tvmaze.com/search/shows?q=${searchTerm}`
+	);
+	console.log(response.data[0].show.image.medium);
+	const img = document.createElement("IMG");
+	img.src = res.data[0].show.image.medium;
+	document.body.append(img);
 });
