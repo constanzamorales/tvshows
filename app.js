04ff2548c09c12c12a395898f1f8afc2379cfd59
constanzamorales <constanzabmorales@gmail.com>
2022-11-12 11:49:54 -0300

    Added if statement for results that dont have a medium size picture

--- app.js
+++ app.js
@@ -9,8 +9,10 @@ form.addEventListener("submit", async function (e) {
 
 const makeImages = (shows) => {
 	for (let result of shows) {
-		const img = document.createElement("IMG");
-		img.src = result.show.image.medium;
-		document.body.append(img);
+		if (result.show.image) {
+			const img = document.createElement("IMG");
+			img.src = result.show.image.medium;
+			document.body.append(img);
+		}
 	}
 };
